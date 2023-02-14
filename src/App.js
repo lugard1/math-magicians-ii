440b7887e119ac6213943581dc56c2663b91933d
Lugard Agu <agulugard@gmail.com>
2023-02-14 12:55:14 +0100

    update component file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Calculator from './components/calculator';
+import Calculator from './components/Calculator';
 import './App.css';
 
 class App extends React.PureComponent {
