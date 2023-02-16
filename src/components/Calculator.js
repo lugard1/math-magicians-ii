1dc84445bddf7ac5f70e1f019b5fa548609d8f63
Lugard Agu <agulugard@gmail.com>
2023-02-16 10:18:47 +0100

    update calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,95 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+function Calculator() {
+  const [Computation, setComputation] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  clickReceived = (event) => {
-    const answer = calculate(this.state, event.target.innerText);
-    this.setState((prevState) => ({
-      ...prevState,
-      ...answer,
-    }));
+  const clickReceived = (event) => {
+    const answer = calculate(event.target.innerText);
+    setComputation(answer);
   };
 
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div className="calculator">
-        <div className="calculator-display">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div className="operations">
-          <button type="button" onClick={this.clickReceived}>
-            AC
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            +/-
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            %
-          </button>
-          <button type="button" onClick={this.clickReceived} className="operators">
-            &#247;
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            7
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            8
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            9
-          </button>
-          <button type="button" onClick={this.clickReceived} className="operators">
-            x
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            4
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            5
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            6
-          </button>
-          <button type="button" onClick={this.clickReceived} className="operators">
-            -
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            1
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            2
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            3
-          </button>
-          <button type="button" onClick={this.clickReceived} className="operators">
-            +
-          </button>
-          <button type="button" onClick={this.clickReceived} className="zero">
-            0
-          </button>
-          <button type="button" onClick={this.clickReceived}>
-            .
-          </button>
-          <button type="button" onClick={this.clickReceived} className="operators">
-            =
-          </button>
-        </div>
+  const { total, operation, next } = Computation;
+  return (
+    <div className="calculator">
+      <div className="calculator-display">
+        {total}
+        {operation}
+        {next}
       </div>
-    );
-  }
+      <div className="operations">
+        <button type="button" onClick={clickReceived}>
+          AC
+        </button>
+        <button type="button" onClick={clickReceived}>
+          +/-
+        </button>
+        <button type="button" onClick={clickReceived}>
+          %
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          &#247;
+        </button>
+        <button type="button" onClick={clickReceived}>
+          7
+        </button>
+        <button type="button" onClick={clickReceived}>
+          8
+        </button>
+        <button type="button" onClick={clickReceived}>
+          9
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          x
+        </button>
+        <button type="button" onClick={clickReceived}>
+          4
+        </button>
+        <button type="button" onClick={clickReceived}>
+          5
+        </button>
+        <button type="button" onClick={clickReceived}>
+          6
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          -
+        </button>
+        <button type="button" onClick={clickReceived}>
+          1
+        </button>
+        <button type="button" onClick={clickReceived}>
+          2
+        </button>
+        <button type="button" onClick={clickReceived}>
+          3
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          +
+        </button>
+        <button type="button" onClick={clickReceived} className="zero">
+          0
+        </button>
+        <button type="button" onClick={clickReceived}>
+          .
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          =
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Calculator;
