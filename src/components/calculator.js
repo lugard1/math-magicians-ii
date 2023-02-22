15caf1f45f5dd1da8ba5010ec82d8c12bc627a35
Lugard Agu <agulugard@gmail.com>
2023-02-22 15:39:02 +0100

    update calculator

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import calculate from '../logic/calculate';
-import Btn from './buttons';
 
 const Calculator = () => {
   const [calculation, setCalculation] = useState(0);
@@ -12,35 +11,70 @@ const Calculator = () => {
 
   const { total, operation, next } = calculation;
   return (
-    <div className="calculator_flex">
-      <h2 className="request">Let&apos;s do some math!</h2>
-      <div className="calculator">
-        <div className="calculator-display">
-          {total}
-          {operation}
-          {next}
-        </div>
-        <div className="operations">
-          <Btn click={clickReceived} className="default" text="AC" />
-          <Btn click={clickReceived} className="default" text="+/-" />
-          <Btn click={clickReceived} className="default" text="%" />
-          <Btn click={clickReceived} className="operators" text="&#247;" />
-          <Btn click={clickReceived} className="default" text="7" />
-          <Btn click={clickReceived} className="default" text="8" />
-          <Btn click={clickReceived} className="default" text="9" />
-          <Btn click={clickReceived} className="operators" text="x" />
-          <Btn click={clickReceived} className="default" text="4" />
-          <Btn click={clickReceived} className="default" text="5" />
-          <Btn click={clickReceived} className="default" text="6" />
-          <Btn click={clickReceived} className="operators" text="-" />
-          <Btn click={clickReceived} className="default" text="1" />
-          <Btn click={clickReceived} className="default" text="2" />
-          <Btn click={clickReceived} className="default" text="3" />
-          <Btn click={clickReceived} className="operators" text="+" />
-          <Btn click={clickReceived} className="zero" text="0" />
-          <Btn click={clickReceived} className="default" text="." />
-          <Btn click={clickReceived} className="operators" text="=" />
-        </div>
+    <div className="calculator">
+      <div className="calculator-display">
+        {total}
+        {operation}
+        {next}
+      </div>
+      <div className="operations">
+        <button type="button" onClick={clickReceived}>
+          AC
+        </button>
+        <button type="button" onClick={clickReceived}>
+          +/-
+        </button>
+        <button type="button" onClick={clickReceived}>
+          %
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          &#247;
+        </button>
+        <button type="button" onClick={clickReceived}>
+          7
+        </button>
+        <button type="button" onClick={clickReceived}>
+          8
+        </button>
+        <button type="button" onClick={clickReceived}>
+          9
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          x
+        </button>
+        <button type="button" onClick={clickReceived}>
+          4
+        </button>
+        <button type="button" onClick={clickReceived}>
+          5
+        </button>
+        <button type="button" onClick={clickReceived}>
+          6
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          -
+        </button>
+        <button type="button" onClick={clickReceived}>
+          1
+        </button>
+        <button type="button" onClick={clickReceived}>
+          2
+        </button>
+        <button type="button" onClick={clickReceived}>
+          3
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          +
+        </button>
+        <button type="button" onClick={clickReceived} className="zero">
+          0
+        </button>
+        <button type="button" onClick={clickReceived}>
+          .
+        </button>
+        <button type="button" onClick={clickReceived} className="operators">
+          =
+        </button>
       </div>
     </div>
   );
